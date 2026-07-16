7658d4a356b6fefb4ad9d95090de2108a5bfeacf
Yotam Saadon <yotamsaad12@gmail.com>
2026-07-16 23:06:55 +0300

    change the ip listen method

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import { config } from './config';
 
 const app = createApp();
 
-app.listen(config.port, () => {
+app.listen(config.port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
-  console.log(`OrderVN Convertor API listening on http://localhost:${config.port}`);
+  console.log(`OrderVN Convertor API listening on http://0.0.0.0:${config.port}`);
   // eslint-disable-next-line no-console
-  console.log(`Swagger UI available at http://localhost:${config.port}/api-docs`);
+  console.log(`Swagger UI available at http://0.0.0.0:${config.port}/api-docs`);
 });
